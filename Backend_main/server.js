c41726cb142b7d5c0202ab9f87639093ddd0be1d
Harikalyan <harikalyan99tronics@gmail.com>
2024-09-29 22:43:02 +0530

    imported express

--- Backend_main/server.js
+++ Backend_main/server.js
@@ -1,7 +1,19 @@
 import dotenv from 'dotenv';
 import connectToMongo from './DB/connectToMongo.js';
 dotenv.config();
+import express from 'express';
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+app.get("/", (request, response) => {
+    return response.send("<h1>Hello from node-express server</h1>")
+})
 
 
 
-connectToMongo();
+app.listen(PORT, () => {
+    console.log(`Listening on the port ${PORT}`)
+    connectToMongo();
+})
+
