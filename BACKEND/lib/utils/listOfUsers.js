faaa7342b247ade32cbb005155cbea0247688d01
Hari kalyan <hari.kalyan.30.1@mountblue.tech>
2024-09-29 22:26:52 +0530

    removed backend

--- BACKEND/lib/utils/listOfUsers.js
+++
@@ -1,6 +0,0 @@
-import AuthUserModel from "../../models/authUser.models.js"
-
-export const listOfUsers = async() => {
-    return await AuthUserModel.find({});
-}
-
--- BACKEND_MAIN/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-
-router.get("/signup", postSignup);
-
-
-export default router;
